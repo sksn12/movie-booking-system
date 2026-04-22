7eb0ae3c70c62a8d5bb1c615e7414de58af49aec
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2026-04-22 17:36:28 +0900

    feat: delete selectable

--- src/movie/MovieDTO.java
+++ src/movie/MovieDTO.java
@@ -4,15 +4,14 @@ import java.time.LocalDateTime;
 
 public class MovieDTO {
 
-	private String movieId;
-	private String title;
-	private String genre;
-	private int runningTime;
-	private int theaterNo;
-	private LocalDateTime startTime;
-	private LocalDateTime endTime;
-	private int totalSeatCount;
-	private int price;
-	private boolean isSelectable;
+    private String movieId;
+    private String title;
+    private String genre;
+    private int runningTime;
+    private int theaterNo;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private int totalSeatCount;
+    private int price;
 
 }
